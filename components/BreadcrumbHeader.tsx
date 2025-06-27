08868a7101ecac4949a42c202c365dedbb4e2842
JP3000 <2691967523@qq.com>
2025-06-27 16:10:10 +0800

    Project setup

---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from './ui/breadcrumb';
+import MODERN_BROWSERSLIST_TARGET from 'next/dist/shared/lib/modern-browserslist-target';
+import { MobileSidebar } from './Sidebar';
+
+function BreadcrumbHeader() {
+    const pathName = usePathname();
+    const paths = pathName === "/" ? [""] : pathName?.split("/");
+  return (
+    <div className='flex items-center flex-start'>
+      <MobileSidebar />
+        <Breadcrumb>
+          <BreadcrumbList>
+            {paths.map((path, index) => (
+                <React.Fragment key={index}>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink className='capitalize' href={`/${path}`}>
+                      {path === "" ? "Home" : path}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </React.Fragment>
+            ))}
+          </BreadcrumbList>
+        </Breadcrumb>
+    </div>
+  )
+}
+
+export default BreadcrumbHeader
